236026dd65b6282e34219fe715980ae99add4560
UhMayYa <48530180+UhMayYa@users.noreply.github.com>
2019-03-22 01:36:15 +0530

    Updated MainActivity
    Added Google Sign in for first part

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,6 +13,14 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -21,6 +29,9 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class  MainActivity extends AppCompatActivity{
 
+     int RC_SIGN_IN=0;
+         SignInButton signInButton;
+    GoogleSignInClient mGoogleSignInClient;
     EditText email,password;
     Button login,signup;
     ImageView homelogo;
@@ -33,6 +44,18 @@ public class  MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         signInButton=findViewById(R.id.sign_in_button);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+  
         email=(EditText)findViewById(R.id.editText);
         password=(EditText)findViewById(R.id.editText2);
         login=(Button)findViewById(R.id.button2);
@@ -85,6 +108,33 @@ public class  MainActivity extends AppCompatActivity{
             }
         });
     }
+     private void signIn()
+    {
+       Intent signInIntent=mGoogleSignInClient.getSignInIntent();
+       startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==RC_SIGN_IN)
+        {
+            Task<GoogleSignInAccount> task=GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask)
+    {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            startActivity(new Intent(MainActivity.this, Main3Activity.class));
+        }catch(ApiException e)
+        {
+            Log.w("Error","Sign in result failed, error code:" + e.getStatusCode());
+            Toast.makeText(MainActivity.this,"Failed",Toast.LENGTH_LONG).show();
+        }
+    }
+
 
 
 
